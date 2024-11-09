8faa7d0ac04dda008df125cab6dec952a67e97a6
MrInmortal434 <118313004+MrInmortal@users.noreply.github.com>
2024-11-08 23:05:10 -0400

    Actualización

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Tarea7.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
